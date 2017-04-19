da7691c9e2f503a5167dd7cf291a965e27bfa6ed
Raven <ruwen1984@qq.com>
2017-04-19 20:01:24 +0800

    创建了Test.java

---
+++ src/edu/guet/dept7/Test.java
@@ -0,0 +1,19 @@
+/*
+ *
+ * 
+ * */
+
+
+package edu.guet.dept7;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1000;
+		int b = 2000;
+		
+		System.out.println("a + b = " + (a + b));
+	}
+
+}
